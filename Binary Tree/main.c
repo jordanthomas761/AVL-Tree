510630e4c17a4119ff5e60baec3ef3d038682681
Jordan Thomas <jordanthomas761@gmail.com>
2014-10-24 15:40:41 -0400

    Initial Commit

---
+++ Binary Tree/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Binary Tree
+//
+//  Created by Jordan Thomas on 10/24/14.
+//  Copyright (c) 2014 Jordan Thomas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
